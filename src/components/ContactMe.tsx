6e87eed286dce680b0a368b71590b2fbca18351b
narsis-ivy-abbasi <narsis.ivy.abbasi@gmail.com>
2025-05-03 14:39:43 +0330

    fixed the issue

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -4,7 +4,6 @@ import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
 import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
 import CircularProgress from "@mui/material/CircularProgress";
