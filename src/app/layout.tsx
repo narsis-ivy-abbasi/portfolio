dae0d875d19d785bd8e4119ab1aaf4112359345f
narsis-ivy-abbasi <narsis.ivy.abbasi@gmail.com>
2025-05-27 18:04:23 +0330

    test hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header>
+          <Hotjar />
+        </header>
         <ThemeRegistry>{children}</ThemeRegistry>
-        <Hotjar />
       </body>
     </html>
   );
