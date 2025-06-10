f14b36c48efab7ea07807d6df1eb5bbd3f40f019
narsis-ivy-abbasi <narsis.ivy.abbasi@gmail.com>
2025-06-10 12:21:53 +0330

    add google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L7NFPNLBKK"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L7NFPNLBKK');
+            `,
+          }}
+        />
+      </head>
       <body>
         <ThemeRegistry>{children}</ThemeRegistry>
       </body>
